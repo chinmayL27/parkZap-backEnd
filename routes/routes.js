39f79dafe8bbb7f03356c5212648b90a7530bd1a
chinmay lohani <chinmaylohani27@gmail.com>
2022-04-09 11:55:02 +0530

    added-get-route

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const userController = require("../controller/userController");
 const formController = require("../controller/formController");
 
+router.route("/").get((req, res) => {
+  res.send("backend_deployed");
+});
 router.route("/user-form").post(userController.userInput); // requirement number 1 {user-form input}
 router.route("/showForms").get(formController.getAllForms); // requirement number 5 {all form show}
 
